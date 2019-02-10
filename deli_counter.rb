2513cddeecd9d28e2544bfb014bbae66bd6db0cb
Lige <lige.williamson@gmail.com>
2019-02-10 15:49:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,7 @@ def take_a_number(people, name)
   people << name
   puts "Welcome, #{name}. You are number #{people.size} in line."
 end
+
+def now_serving(people)
+  if people == []
+    puts "The line is empty."
